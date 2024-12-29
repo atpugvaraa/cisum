552539272ede361d53feac882426a2ff3df79c7e
atpugvaraa <atpugvaraa@gmail.com>
2024-12-29 15:27:11 +0530

    Project Setup
    Setting up the project via Xcode

---
+++ cisum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cisum
+//
+//  Created by Aarav Gupta on 29/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cisum/cisumApp.swift
@@ -0,0 +1,17 @@
+//
+//  cisumApp.swift
+//  cisum
+//
+//  Created by Aarav Gupta on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct cisumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
