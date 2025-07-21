6bf9ae05598cb3fd23ae52e92de525b56d8cfd3f
Aarav Gupta <atpugvaraa@gmail.com>
2025-07-21 08:56:49 +0530

    New beginnin's, ahh - Frank Ocean

---
+++ cisum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cisum
+//
+//  Created by Aarav Gupta on 21/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cisum/cisumApp.swift
@@ -0,0 +1,17 @@
+//
+//  cisumApp.swift
+//  cisum
+//
+//  Created by Aarav Gupta on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct cisumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
